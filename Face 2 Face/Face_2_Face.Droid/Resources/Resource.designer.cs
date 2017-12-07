4616f93b8c71788d53732999510f77da21ddb148
nuwanwixx <nuwan.wixx@gmail.com>
2017-12-07 16:53:26 +0530

    Change apperance of Buttons on login layout

--- Face 2 Face/Face_2_Face.Droid/Resources/Resource.designer.cs
+++ Face 2 Face/Face_2_Face.Droid/Resources/Resource.designer.cs
@@ -60,14 +60,20 @@ namespace Face_2_Face.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int BtnLogin = 2131034115;
-			
 			// aapt resource value: 0x7f050005
-			public const int BtnSignUp = 2131034117;
+			public const int BtnLogin = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int BtnSignUp = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int btnForgot = 2131034118;
+			
+			// aapt resource value: 0x7f050002
+			public const int editText1 = 2131034114;
 			
 			// aapt resource value: 0x7f050004
-			public const int btnForgot = 2131034116;
+			public const int editText2 = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
@@ -75,8 +81,8 @@ namespace Face_2_Face.Droid
 			// aapt resource value: 0x7f050001
 			public const int textView2 = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int textView3 = 2131034114;
+			// aapt resource value: 0x7f050003
+			public const int textView3 = 2131034115;
 			
 			static Id()
 			{
